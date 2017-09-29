2cd7a7fd18f6177351bbf0832408686801652c2a
Mario Aburto <marioaburto10@gmail.com>
2017-09-28 23:58:30 -0400

    burger can now be devoured, i.e. its eaten value can now be updated from 0 to 1

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -20,9 +20,11 @@ router.post('/burgers/create', function(req, res){
 
 });
 
-router.put('/burgers/update/:ID', function(req, res){
-  burger.updateOne([req.body.devoured], [req.params.ID], function(){
+router.put('/burgers/update/:id', function(req, res){
+  console.log(req.body.devoured);
+  burger.updateOne([req.body.devoured], [req.params.id], function(){
     res.redirect('/burgers');
+    // res.send("burger has been eaten");
   });
 });
 
